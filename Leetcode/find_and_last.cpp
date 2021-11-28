41da93e92ff3c40d7d57bc961f96bb9cf3991b58
akshatnema <20bcs022@iiitdmj.ac.in>
2021-11-29 00:18:10 +0530

    find_and_last
    Signed-off-by: akshatnema <20bcs022@iiitdmj.ac.in>

---
+++ Leetcode/find_and_last.cpp
@@ -0,0 +1,68 @@
+// Question link - https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+       if(nums.size()==0){
+           vector<int> v;
+           v.push_back(-1);
+           v.push_back(-1);
+           return v;
+       } 
+       int l=0,r=nums.size()-1;
+        int mi=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==target){
+                if(mid>0){
+                    if(nums[mid]==nums[mid-1])
+                        r=mid-1;
+                    else{
+                        mi=mid;
+                        break;
+                    }
+                }
+                else{
+                    mi=mid;
+                    break;
+                }
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else 
+                r=mid-1;
+        }
+        int mx=-1;
+         l=0,r=nums.size()-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==target){
+                if(mid<nums.size()-1){
+                    if(nums[mid]==nums[mid+1])
+                        l=mid+1;
+                    else{
+                        mx=mid;
+                        break;
+                    }
+                }
+                else{
+                    mx=mid;
+                    break;
+                }
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else 
+                r=mid-1;
+        }
+        vector <int> v;
+        v.push_back(mi);
+        v.push_back(mx);
+        return v;
+    }
+};
