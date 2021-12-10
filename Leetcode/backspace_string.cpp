bb99f4e0fd4336fb03510c03c51353c924a1807a
akshatnema <20bcs022@iiitdmj.ac.in>
2021-12-10 15:25:39 +0530

    backspace_string.cpp

---
+++ Leetcode/backspace_string.cpp
@@ -0,0 +1,43 @@
+// Question Link - https://leetcode.com/problems/backspace-string-compare/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack <char> s1;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='#'){
+                if(!s1.empty())
+                    s1.pop();
+                continue;
+            }
+            s1.push(s[i]);
+        }
+        s="";
+        while(!s1.empty()) {
+            s+=s1.top();
+            s1.pop();
+        }
+
+        for(int i=0;i<t.length();i++){
+            if(t[i]=='#'){
+                if(!s1.empty())
+                    s1.pop();
+                continue;
+            }
+            s1.push(t[i]);
+        }
+        t="";
+        while(!s1.empty()) {
+            t+=s1.top();
+            s1.pop();
+        }
+        // cout<<s<<endl;
+        // cout<<t<<endl;
+        if(s==t)
+            return true;
+        else return false;
+    }
+};
