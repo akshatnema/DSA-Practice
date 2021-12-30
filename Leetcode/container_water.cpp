5ae318eb921a2a26bb4b92b7ffc9f4049056319b
akshatnema <20bcs022@iiitdmj.ac.in>
2021-12-30 12:44:49 +0530

    container with most water

---
+++ Leetcode/container_water.cpp
@@ -0,0 +1,26 @@
+// Question link - https://leetcode.com/problems/container-with-most-water/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0;
+        int r=height.size()-1;
+        int area=-1;
+        while(l<r){
+            int mi=min(height[l],height[r]);
+            int in=r-l;
+            int temp=mi*in;
+            area=max(area,temp);
+            if(height[l]<height[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return area;
+    }
+};
