688e7d59380f2e77f71b9302fefbe749dec23103
Akshat Nema <76521428+akshatnema@users.noreply.github.com>
2023-01-15 12:27:46 +0530

    Time: 4 ms (61.72%), Space: 13.6 MB (22.21%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     set<vector<int>> s;
-    void rec(int i, vector<int> &nums, vector<int> temp){
+    void rec(int i, vector<int> &nums, vector<int> &temp){
         if(i==nums.size()){
             s.insert(temp);
             return ;
@@ -9,6 +9,7 @@ public:
         rec(i+1, nums, temp);
         temp.push_back(nums[i]);
         rec(i+1, nums, temp);
+        temp.pop_back();
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
