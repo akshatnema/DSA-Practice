29add179aa5784580f2827572b9e3f3abdc12285
Akshat Nema <76521428+akshatnema@users.noreply.github.com>
2023-07-20 12:53:41 +0530

    Time: 283 ms (8.17%), Space: 167.1 MB (6.86%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -8,27 +8,27 @@ public:
         return s1==s;
     }
     
-    void rec(string s, string temp, vector<string> partitions, int i) {
+    void rec(string s, vector<string> partitions, int i) {
         if(i>s.length()) return ;
-        else if(i==s.length() && !temp.length()){
+        else if(i==s.length()){
             ans.push_back(partitions);
             return ;
         } else {}
-        string temp1 = temp + s[i];
-        if(isPalindrome(temp1)) {
-            partitions.push_back(temp1);
-            rec(s, "", partitions, i+1);
-            partitions.pop_back();
-            rec(s, temp1, partitions, i+1);
-        } else {
-            rec(s, temp1, partitions, i+1);
+        for(int index=i;index<s.length();index++) {
+            string temp1=s.substr(i, index-i+1);
+            
+            if(isPalindrome(temp1)) {
+                partitions.push_back(temp1);
+                rec(s, partitions, index+1);
+                partitions.pop_back();
+            }
         }
     }
     
     vector<vector<string>> partition(string s) {
         vector<string> partitions;
         string temp="";
-        rec(s, temp, partitions, 0);
+        rec(s, partitions, 0);
         return ans;
     }
 };
